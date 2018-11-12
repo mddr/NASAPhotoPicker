229b74a19b54e16ba512bc9e82eababb4d99bdcb
PanKaczka <kapoludek@gmail.com>
2018-11-12 20:46:03 +0100

    modal

--- blog-controller/blog-controller.js
+++ blog-controller/blog-controller.js
@@ -1,4 +1,20 @@
 angular.module("NASAPhotoPicker")
-    .controller('blogController', function($scope) {
+    .controller('blogController', ["$scope", 'ModalService', function($scope, ModalService) {
+        $scope.openModal = openModal;
+        $scope.closeModal = closeModal;
         
-    });
+        initController();
+
+        function initController() {
+            $scope.bodyText = 'This text can be updated in modal 1';
+        }
+
+        function openModal(id){
+            ModalService.open(id);
+        }
+
+        function closeModal(id){
+            ModalService.close(id);
+        }
+        
+    }]);
